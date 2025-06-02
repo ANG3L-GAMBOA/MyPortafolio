4f7b4eac3a0f51b13391453c850e9510b7c14823
ANG3L-GAMBOA <angelxdangerxdgamara@gmail.com>
2025-06-02 16:13:20 -0500

    v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://mywebsitecollab.vercel.app/'
-})
+  site: "https://mywebsitecollab.vercel.app/",
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+    fallback: {
+      en: "es",
+    },
+  },
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,27 @@
+export const languages = {
+  //es: "Español",
+  //en: "English",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "nav.experiencia": "Experiencia",
+    "nav.educacion": "Educación",
+    "nav.proyectos": "Proyectos",
+    "nav.habilidades": "Habilidades",
+    "nav.certificados": "Certficados",
+    "nav.sobre-mi": "Sobre mí",
+    "nav.contacto": "Contacto",
+  },
+  en: {
+    "nav.experiencia": "Experience",
+    "nav.educacion": "Education",
+    "nav.proyectos": "Projects",
+    "nav.habilidades": "Skills",
+    "nav.certificados": "Certificates",
+    "nav.sobre-mi": "About me",
+    "nav.contacto": "Contact",
+  },
+} as const;
